75212d62cd048e6a14a479cfc5bdb51ccd41c885
GIOVANYSTIVEN <steventu06@gmail.com>
2025-03-19 11:21:53 -0500

    sensores sin admin y canbio navbar

--- Agrosis_API-dev/apps/Iot/sensores/api/views.py
+++ Agrosis_API-dev/apps/Iot/sensores/api/views.py
@@ -3,11 +3,10 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 from ..models import Sensores
 from .serializers import SensoresSerializer
-from apps.Usuarios.usuarios.api.permissions import IsAdminOrRead
 
 class SensoresViewset(ModelViewSet):
     authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated,IsAdminOrRead ]
+    permission_classes = [IsAuthenticated, ]
     queryset = Sensores.objects.all()
     serializer_class = SensoresSerializer
     
