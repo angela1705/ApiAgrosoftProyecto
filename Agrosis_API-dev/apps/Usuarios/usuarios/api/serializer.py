2036d9322e4e540a62f3c5708a7fd79ba26be47d
juancc9 <sicsound00@gmail.com>
2025-03-27 18:32:53 -0500

    "Resolvi error de registro de usuarios"

--- Agrosis_API-dev/apps/Usuarios/usuarios/api/serializer.py
+++ Agrosis_API-dev/apps/Usuarios/usuarios/api/serializer.py
@@ -8,31 +8,29 @@ from apps.Usuarios.roles.api.serializer import RolSerializer
 class UsuariosSerializer(serializers.ModelSerializer):
     rol = RolSerializer(read_only=True) 
     rol_id = serializers.PrimaryKeyRelatedField(
-        queryset=Roles.objects.all(), source="rol", write_only=True, required=False, allow_null=True
+        queryset=Roles.objects.all(), source="rol", write_only=True, allow_null=True
     ) 
 
     class Meta:
         model = Usuarios
-        fields = ['id', 'nombre', 'apellido', 'email', 'username', 'rol', 'rol_id','password']
+        fields = ['id', 'nombre', 'apellido', 'email', 'username', 'rol', 'rol_id']
 class RegistroUsuarioSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
 
     class Meta:
         model = Usuarios
-        fields = ['id', 'nombre', 'apellido', 'email', 'username',  'password']
+        fields = ['id', 'nombre', 'apellido', 'email', 'username', 'rol', 'password']
 
     def create(self, validated_data):
-        # Asignar un rol por defecto si no se proporciona
-        if 'rol' not in validated_data or validated_data['rol'] is None:
-            try:
-                default_role = Roles.objects.get(nombre='aprendiz')
-                validated_data['rol'] = default_role
-            except ObjectDoesNotExist:
-                raise serializers.ValidationError("El rol por defecto 'aprendiz' no existe en la base de datos.")
         
-        # Crear el usuario
+        if 'password' not in validated_data:
+            raise serializers.ValidationError({"password": "Este campo es obligatorio."})
+        
         password = validated_data.pop('password')
         usuario = Usuarios(**validated_data)
-        usuario.set_password(password)
+        
+        usuario.set_password(password)  # Encripta la contraseña
+        
+        
         usuario.save()
         return usuario
