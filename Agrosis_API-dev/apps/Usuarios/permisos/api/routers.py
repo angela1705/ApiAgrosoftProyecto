f9d030047fa3041b52ab557d91822258e6ad128a
juancc9 <sicsound00@gmail.com>
2025-03-30 08:45:49 -0500

    "Cambios en el backend de usuarios"

--- Agrosis_API-dev/apps/Usuarios/permisos/api/routers.py
+++
@@ -1,5 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from apps.Usuarios.permisos.api.views import PermisosViewSet
-
-PermisosRouter = DefaultRouter()
-PermisosRouter.register(prefix='permisos', viewset=PermisosViewSet, basename='permisos')
--- Agrosis_API-dev/apps/Usuarios/usuario_rol/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from apps.Usuarios.usuarios.models import Usuarios
-from apps.Usuarios.usuarios.models import Roles
-
-class UsuarioRol(models.Model):
-    usuario = models.ForeignKey(Usuarios, on_delete=models.CASCADE)
-    rol = models.ForeignKey(Roles, on_delete=models.CASCADE)
-
-# Create your models here.
