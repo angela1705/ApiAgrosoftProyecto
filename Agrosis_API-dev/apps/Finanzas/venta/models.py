2e75d2c752510c6ae11427fe26e3d25d8b149c97
yhuliaxl <yhuliaxl@gmail.com>
2025-04-03 18:41:51 -0500

    Modificacion de la tabla ventas

--- Agrosis_API-dev/apps/Finanzas/venta/models.py
+++ Agrosis_API-dev/apps/Finanzas/venta/models.py
@@ -1,17 +1,21 @@
 from django.db import models
+from apps.Cultivo.cultivos.models import Cultivo
 
 class Venta(models.Model):
     producto = models.ForeignKey('cultivos.Cultivo', on_delete=models.CASCADE)
     cantidad = models.PositiveIntegerField()
     total = models.DecimalField(max_digits=10, decimal_places=2, editable=False)
     fecha = models.DateTimeField(auto_now_add=True)
-    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    fk_cosecha = models.ForeignKey('cosechas.Cosecha', on_delete=models.CASCADE, null=True, blank=True)  # Agrega null=True y blank=True
 
     def save(self, *args, **kwargs):
-        self.total = self.precio * self.cantidad
+        if self.fk_cosecha:  # Verifica si fk_cosecha existe
+            self.total = self.cantidad * self.fk_cosecha.cantidad  # Placeholder, ajusta según tu lógica
+        else:
+            self.total = 0  # O ajusta según tu lógica si fk_cosecha es None
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return f"Venta de {self.producto.nombre} ({self.cantidad} unidades)"
+        return f"Venta de {self.producto.nombre} ({self.cantidad} unidades) - Cosecha {self.fk_cosecha.fecha if self.fk_cosecha else 'Sin cosecha'}"
 
 # Create your models here.
