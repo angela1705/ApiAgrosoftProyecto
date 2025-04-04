d65eb732df7c9c4f513410766bdc42877cf985cf
WMauricioDev <oscar_audor@soy.sena.edu.co>
2025-04-03 21:18:56 -0500

    solucion url de usereporte

--- Agrosis_API-dev/apps/Cultivo/bancal/models.py
+++ Agrosis_API-dev/apps/Cultivo/bancal/models.py
@@ -5,6 +5,6 @@ class Bancal(models.Model):
     TamY = models.DecimalField(max_digits=3, decimal_places=2, null=True, blank=True)
     posY = models.DecimalField(max_digits=3, decimal_places=2, null=True, blank=True)
     posX = models.DecimalField(max_digits=3, decimal_places=2, null=True, blank=True)
-    lote = models.ForeignKey('lotes.Lote', on_delete=models.CASCADE, null=True)
+    lote = models.ForeignKey('lotes.Lote', on_delete=models.CASCADE)
     def __str__(self):
         return self.nombre
--- Frontend/src/types/reportes/reportes.ts
+++ Frontend/src/types/reportes/reportes.ts
@@ -11,8 +11,7 @@ export const reportesPorModulo: Record<string, { nombre: string; reporte: string
         { nombre: "Reporte de Cosechas", reporte: "cosechas" },
         { nombre: "Reporte de lotes", reporte: "lote" },
         { nombre: "Reporte de actividades", reporte: "actividades"},
-
-
+        { nombre: "Reporte de actividades", reporte: "Bancal"},
     ],
     inventario: [
         { nombre: "Reporte de Herramientas", reporte: "herramientas" },
