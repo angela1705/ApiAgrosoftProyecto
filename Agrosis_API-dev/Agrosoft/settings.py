9357556e8fbb9211ae27d763afb028b60cd1233e
WMauricioDev <oscar_audor@soy.sena.edu.co>
2025-04-19 00:43:39 -0500

    se implemento la busqueda de herramientas, insumos, usuarios y dar finalizacion a una actividad

--- Agrosis_API-dev/Agrosoft/settings.py
+++ Agrosis_API-dev/Agrosoft/settings.py
@@ -135,7 +135,7 @@ CORS_ALLOWED_ORIGINS = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'agrosoft_leo',
+        'NAME': 'testdb1',
         'USER': 'postgres',
         'PASSWORD': 'root',
         'HOST': 'localhost', 
