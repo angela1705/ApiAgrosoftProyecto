3f3934f933865176321a8195239857baafcbd293
juancc9 <sicsound00@gmail.com>
2025-04-30 23:43:47 -0500

    "Correciones Autenticacion"

--- Agrosis_API-dev/Agrosoft/settings.py
+++ Agrosis_API-dev/Agrosoft/settings.py
@@ -82,12 +82,10 @@ INSTALLED_APPS = [
     'django_filters',
     'apps.Iot.evapotranspiracion',
 ]
-
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'apps.Autenticacion.autenticacion.middleware.amJWTAuthFromCookieMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -139,9 +137,9 @@ CORS_ALLOWED_ORIGINS = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'testdb1',
+        'NAME': 'agrosoft',
         'USER': 'postgres',
-        'PASSWORD': 'root',
+        'PASSWORD': 'pass',
         'HOST': 'localhost', 
         'PORT': '5432',
         'OPTIONS': {
@@ -203,7 +201,6 @@ SIMPLE_JWT = {
 
 AUTH_USER_MODEL = 'usuarios.Usuarios'
 AUTHENTICATION_BACKENDS = [
-    'apps.Autenticacion.autenticacion.backends.EmailBackend',
     'django.contrib.auth.backends.ModelBackend',
 ]
 
@@ -232,7 +229,6 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'apps.Autenticacion.autenticacion.authentication.CookieJWTAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
