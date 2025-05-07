c07fa817523e0eed111644c46459d49ac821315c
MauricioAudor <oscar_audor@soy.sena.edu.co>
2025-05-07 15:07:52 +0200

    se soluciono problemas de migraciones

--- Agrosis_API-dev/apps/Cultivo/actividades/migrations/0004_actividad_estado_actividad_instrucciones_adicionales_and_more.py
+++ Agrosis_API-dev/apps/Cultivo/actividades/migrations/0004_actividad_estado_actividad_instrucciones_adicionales_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('actividades', '0003_remove_actividad_programacion_and_more'),
+        ('actividades', '0001_initial'),
     ]
 
     operations = [
