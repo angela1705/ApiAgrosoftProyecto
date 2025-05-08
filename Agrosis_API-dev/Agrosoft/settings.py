d3863c25dfc19ffc74315856ed31b3a3c23ffa13
Haisonleandro <haisonleandrotoro@gmail.com>
2025-05-07 20:36:17 -0500

    modificamos el diseño de precio producto creamos el componente para el modal cosechas

--- Agrosis_API-dev/Agrosoft/settings.py
+++ Agrosis_API-dev/Agrosoft/settings.py
@@ -133,7 +133,7 @@ CORS_ALLOWED_ORIGINS = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'testdb5',
+        'NAME': 'agrosoft_new2',
         'USER': 'postgres',
         'PASSWORD': 'root',
         'HOST': 'localhost', 
