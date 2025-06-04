c94c00238c252df6d85eb61d74218b62f7efeedc
GIOVANYSTIVEN <steventu06@gmail.com>
2025-06-03 19:50:48 -0500

    Este sí, para que el header conserve bien la unión con el navbar.

--- Agrosis_API-dev/Agrosoft/settings.py
+++ Agrosis_API-dev/Agrosoft/settings.py
@@ -137,7 +137,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql', 
         'NAME': 'agrosoft8', 
         'USER': 'postgres',
-        'PASSWORD': 'root',
+        'PASSWORD': 'adso2024',
         'HOST': 'localhost', 
         'PORT': '5432',
         'OPTIONS': {
