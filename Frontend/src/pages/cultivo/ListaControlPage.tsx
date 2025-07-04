6842daddbfa12455aeacb78edeaf357e76b3d8a8
WMauricioDev <oscar_audor@soy.sena.edu.co>
2025-07-03 22:36:24 -0500

    docker correcion v2

--- Frontend/src/pages/cultivo/ListaControlPage.tsx
+++ Frontend/src/pages/cultivo/ListaControlPage.tsx
@@ -75,7 +75,7 @@ const ListaControlPage: React.FC = () => {
             control.efectividad >= 70
               ? "bg-green-100 text-green-800"
               : control.efectividad >= 40
-              ? "bg-yellow-100 text-yellow-800"
+              ? "bg-yellow-100 text-yellow-800" 
               : "bg-red-100 text-red-800"
           }`}
         >
