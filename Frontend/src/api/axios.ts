504f236951179d29217f591405e1d67168d54783
WMauricioDev <oscar_audor@soy.sena.edu.co>
2025-07-31 17:57:51 -0500

    docker images 3.0

--- Frontend/src/api/axios.ts
+++ Frontend/src/api/axios.ts
@@ -1,11 +1,15 @@
 import axios from "axios";
 
+const runtimeBaseUrl = import.meta.env.VITE_API_BASE_URL!;
+
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || `${window.location.protocol}//${window.location.hostname}:8000`,
+  baseURL: runtimeBaseUrl,
   headers: {
     "Content-Type": "application/json",
   },
 });
+
 
 api.interceptors.request.use(
   (config) => {
@@ -19,5 +23,8 @@ api.interceptors.request.use(
   },
   (error) => Promise.reject(error)
 );
+console.log(import.meta.env.VITE_API_BASE_URL);
+console.log(`${window.location.protocol}//${window.location.hostname}:8000`);
+
 
 export default api;
